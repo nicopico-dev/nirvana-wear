a22a751ba1f7185ac62ebd6d45449f9611c50f41
Nicolas Picon <nicopico.dev@gmail.com>
2023-05-14 19:26:34 +0200

    build: configure Detekt

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     kotlin("jvm") version "1.8.10"
     `java-library`
     kotlin("plugin.serialization") version "1.8.10"
+
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
 
 repositories {
@@ -9,19 +11,19 @@ repositories {
 }
 
 dependencies {
-    val ktor_version = "2.2.4"
-    val kotest_version = "5.5.5"
+    val ktorVersion = "2.2.4"
+    val kotestVersion = "5.5.5"
 
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-okhttp:$ktor_version")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
-    testImplementation("io.ktor:ktor-client-mock:$ktor_version")
-    testImplementation("io.kotest:kotest-assertions-core:$kotest_version")
+    testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
 }
 
 tasks.named<Test>("test") {
